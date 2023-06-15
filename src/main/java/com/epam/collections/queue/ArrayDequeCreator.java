a8a60dbfc423effb422ff3273af5077b4b2ef780
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-06-15 15:39:22 +0300

    Queue

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,29 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+
+        for (int i = 0; i < 2; i++) {
+            if(!firstQueue.isEmpty())
+                arrayDeque.add(firstQueue.poll());
+        }
+        for (int i = 0; i < 2; i++) {
+            if(!secondQueue.isEmpty())
+                arrayDeque.add(secondQueue.poll());
+        }
+        while(firstQueue.size() != 0 || secondQueue.size() != 0){
+
+            firstQueue.add(arrayDeque.pollLast());
+            for (int i = 0; i < 2; i++) {
+                if(!firstQueue.isEmpty())
+                    arrayDeque.add(firstQueue.poll());
+            }
+            secondQueue.add(arrayDeque.pollLast());
+            for (int i = 0; i < 2; i++) {
+                if(!secondQueue.isEmpty())
+                    arrayDeque.add(secondQueue.poll());
+            }
+        }
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>(numberOfDishes);
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        int c = 0;
+        while (!queue.isEmpty()){
+            int n = queue.poll();
+            c++;
+            if(c % everyDishNumberToEat == 0){
+                list.add(n);
+            } else {
+                queue.add(n);
+            }
+        }
+        return list;
     }
 }
